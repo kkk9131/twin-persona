a55a7e2be14057cffb8f40fac9e4aa77a97fb0c0
Kaz Trigger <k.a52@icloud.com>
2025-08-21 23:00:18 +0900

    fix: Vercel用にbase pathを修正

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/twin-persona/',
+  base: process.env.GITHUB_PAGES ? '/twin-persona/' : '/',
   build: {
     outDir: 'dist',
     sourcemap: false,
